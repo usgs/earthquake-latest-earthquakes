dab65c7c358c78d3d503ac2da3ab006ff5690fb7
ehunter-usgs <ehunter@usgs.gov>
2014-01-23 13:08:55 -0700

    migrate htdocs conf and lib into the src directory.

--- src/htdocs/js/index.js
+++ src/htdocs/js/index.js
@@ -1,14 +1,42 @@
 require.config({
 	baseUrl: 'js',
+	urlArgs: "stamp="+(new Date()).getTime(), /* Remove for production */
 	paths: {
+		leaflet: '../lib/leaflet/leaflet-src',
+		localStorage: '../lib/localStorage',
+		JSON: '../lib/JSON'
 	},
 	shim: {
+		leaflet: {exports: 'L'},
+		localStorage: {exports: 'localStorage'},
+		JSON: {exports: 'JSON'}
 	}
 });
 
-require([
-], function (
-) {
-	'use strict';
-	console.log('Framework Loaded');
-});
+
+
+var _application;
+
+require(
+	["eq/EarthquakeApp"],
+	function(EarthquakeApp) {
+
+		_application = new EarthquakeApp({
+			el: document.getElementById("application")
+		});
+
+	}
+);
+
+
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-7320779-1']);
+_gaq.push(['_gat.anonymizeIp']);
+_gaq.push(['_trackPageview']);
+(function () {
+	var ga = document.createElement('script');
+	ga.async = true;
+	ga.src = 'http://www.google-analytics.com/ga.js';
+	document.querySelector('script').parentNode.appendChild(ga);
+})();
