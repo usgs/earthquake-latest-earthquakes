25a04d608100c9f6da70e1b480ae6eac414f658d
Hal Simpson <hasimpson@usgs.gov>
2014-06-24 15:38:00 -0600

    override leaflet setPosition to allow our transforms for diamond markers

--- src/htdocs/js/eq/MapViewDependencies.js
+++ src/htdocs/js/eq/MapViewDependencies.js
@@ -18,6 +18,36 @@ define([
 ) {
 	'use strict';
 
+	//Patch leaflet with our sourcecode.
+	L.DomUtil.setPosition = function (el, point, disable3D) {
+				var previousTransform = null;
+
+		el._leaflet_pos = point;
+
+		if (!disable3D && L.Browser.any3d) {
+
+		previousTransform = el.style[L.DomUtil.TRANSFORM] || '';
+
+			if (previousTransform.indexOf('translate') !== -1) {
+				// replace existing translate
+				el.style[L.DomUtil.TRANSFORM] = previousTransform.replace(
+					/translate(3d)?\([^\)]+\)/i,
+					L.DomUtil.getTranslateString(point));
+			} else {
+				// insert translate before any other transforms (like rotate)
+				el.style[L.DomUtil.TRANSFORM] = L.DomUtil.getTranslateString(point) + ' ' + previousTransform;
+			}
+
+			// workaround for Android 2/3 stability (https://github.com/CloudMade/Leaflet/issues/69)
+			if (L.Browser.mobileWebkit3d) {
+				el.style.WebkitBackfaceVisibility = 'hidden';
+			}
+		} else {
+			el.style.left = point.x + 'px';
+			el.style.top = point.y + 'px';
+		}
+	};
+
 	// container for MapView dependencies for simpler compilation
 	return {
 		L: L,
