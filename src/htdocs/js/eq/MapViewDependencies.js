7970c654243b06f2ce20512892c45fab1d793f2b
Hal Simpson <hasimpson@usgs.gov>
2014-07-11 13:31:51 -0600

    fixed boxzoom on windows 8.1/ie11 by adding touchevent to the Map.BoxZoom leaflet class, will see if Leaflet will adapt it, if so we can get rid of several of the overridden functions.

--- src/htdocs/js/eq/MapViewDependencies.js
+++ src/htdocs/js/eq/MapViewDependencies.js
@@ -63,7 +63,8 @@ define([
 
 		if ((e.shiftKey || (this._dragStartTarget &&
 			L.DomUtil.hasClass(this._dragStartTarget, 'leaflet-box-zooming'))) ||
-			((e.which !== 1) && (e.button !== 1) && !e.touches)) { return; }
+			((e.which !== 1) && (e.button !== 1) && !e.touches))
+			{ return; }
 
 		L.DomEvent.stopPropagation(e);
 
@@ -84,6 +85,17 @@ define([
 		    .on(document, L.Draggable.END[e.type], this._onUp, this);
 	};
 
+	L.Map.BoxZoom.prototype.addHooks = function () {
+		L.DomEvent.on(this._container, 'mousedown', this._onMouseDown, this);
+		L.DomEvent.on(this._container, 'touchstart', this._onMouseDown, this);
+	};
+
+	L.Map.BoxZoom.prototype.removeHooks = function () {
+		L.DomEvent.off(this._container, 'mousedown', this._onMouseDown);
+		L.DomEvent.off(this._container, 'touchstart', this._onMouseDown);
+		this._moved = false;
+	};
+
 	L.Map.BoxZoom.prototype._onMouseDown = function (e) {
 		this._moved = false;
 
@@ -97,8 +109,51 @@ define([
 
 		L.DomEvent
 		    .on(document, 'mousemove', this._onMouseMove, this)
+		    .on(document, 'touchmove', this._onMouseMove, this)
 		    .on(document, 'mouseup', this._onMouseUp, this)
+		    .on(document, 'touchend', this._onMouseUp, this)
 		    .on(document, 'keydown', this._onKeyDown, this);
+	};
+
+	L.Map.BoxZoom.prototype._onMouseUp = function (e) {
+
+		this._finish();
+
+		var map = this._map,
+		    layerPoint = map.mouseEventToLayerPoint(e);
+
+		if (this._startLayerPoint.equals(layerPoint)) { return; }
+
+		var bounds = new L.LatLngBounds(
+		        map.layerPointToLatLng(this._startLayerPoint),
+		        map.layerPointToLatLng(layerPoint));
+
+		map.fitBounds(bounds);
+
+		if (L.DomUtil.hasClass(this._container, 'leaflet-box-zooming')) {
+			L.DomUtil.removeClass(this._container, 'leaflet-box-zooming');
+		}
+
+		map.fire('boxzoomend', {
+			boxZoomBounds: bounds
+		});
+	};
+
+	L.Map.BoxZoom.prototype._finish = function () {
+		if (this._moved) {
+			this._pane.removeChild(this._box);
+			this._container.style.cursor = '';
+		}
+
+		L.DomUtil.enableTextSelection();
+		L.DomUtil.enableImageDrag();
+
+		L.DomEvent
+		    .off(document, 'mousemove', this._onMouseMove)
+		    .off(document, 'touchmove', this._onMouseMove)
+		    .off(document, 'mouseup', this._onMouseUp)
+		    .off(document, 'touchend', this._onMouseUp)
+		    .off(document, 'keydown', this._onKeyDown);
 	};
 
 
