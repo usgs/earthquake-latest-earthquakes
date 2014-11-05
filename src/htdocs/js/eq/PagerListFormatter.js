9c7f6f56d5b088441389a4e58acb035a5c7edbe0
Hal Simpson <hasimpson@usgs.gov>
2014-11-05 14:40:51 -0700

    minor changes to look more like other ListFormatter's

--- src/htdocs/js/eq/PagerListFormatter.js
+++ src/htdocs/js/eq/PagerListFormatter.js
@@ -1,11 +1,9 @@
 /* global define */
 define ([
   'mvc/Util',
-  './DefaultListFormatter',
   './Format'
 ], function(
   Util,
-  DefaultListFormatter,
   Format
 ){
   'use strict';
@@ -15,21 +13,15 @@ define ([
   };
 
   var PagerListFormatter = function (options) {
-    DefaultListFormatter.apply(this, arguments);
     this._options = Util.extend({},DEFAULTS, options);
   };
-
-  PagerListFormatter.prototype =
-      Object.create(DefaultListFormatter.prototype);
-
 
   PagerListFormatter.prototype.getListClassName = function () {
     return this._options.className;
   };
 
   PagerListFormatter.prototype.generateListItemMarkup = function (item) {
-    var markup = [],
-        prefix = this._options.idprefix,
+    var prefix = this._options.idprefix,
         settings = this._options.settings,
         p, c, highlightClass, alert, alertClass, mmi, mmiClass;
 
@@ -59,24 +51,21 @@ define ([
       highlightClass = ' class="big"';
     }
 
-    markup.push(
-    '<li id="', prefix, item.id, '"', highlightClass, '>',
-      '<span class="',alertClass,'">',
-        alert,
-      '</span> ',
-      '<span class="place">',
-        p.title,
-      '</span> ',
-      '<span class="time"> ',
-        Format.dateFromEvent(item, settings),
-      '</span> ',
-      '<span class = "maxintensity">Maximum Intensity:</span>',
-      '<span class="',mmiClass,'">',
-        mmi,
-      '</span>',
-    '</li>');
-
-    return markup.join('');
+  return '<li id="' + prefix + item.id + '"' + highlightClass + '>' +
+    '<span class="' + alertClass + '">' +
+      alert +
+    '</span> ' +
+    '<span class="place">' +
+      p.title +
+    '</span> ' +
+    '<span class="time"> ' +
+      Format.dateFromEvent(item, settings) +
+    '</span> ' +
+    '<span class = "maxintensity">Maximum Intensity:</span>' +
+    '<span class="' + mmiClass + '">' +
+      mmi +
+    '</span>' +
+    '</li>';
   };
 
   return PagerListFormatter;
