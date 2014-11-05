115a4d7300278e0d833eedd4357e29074b2d456a
Hal Simpson <hasimpson@usgs.gov>
2014-11-05 09:44:28 -0700

    created shakemap list formatter,  and made it as similiar as possible to dyfilistformatter

--- src/htdocs/js/eq/ShakeMapListFormatter.js
+++ src/htdocs/js/eq/ShakeMapListFormatter.js
@@ -1,6 +1,6 @@
 /* global define */
 define ([
-	'mvc/DefaultListFormatter',
+	'./DefaultListFormatter',
 	'mvc/Util',
 	'./Format'
 ], function(
@@ -10,71 +10,93 @@ define ([
 ){
 	'use strict';
 
-	var DEFAULTS = {
+	var ShakeMapListFormatter = function (/*options*/) {
+		DefaultListFormatter.apply(this, arguments);
 	};
 
-	var ShakeMapListFormatter = function (options) {
-		DefaultListFormatter.apply(this,arguments);
+	ShakeMapListFormatter.prototype =
+			Object.create(DefaultListFormatter.prototype);
 
-		this._options = Util.extend({}, DEFAULTS, options);
-		//Util.addClass(this._el, 'listView');
-		this.initialize();
+
+	ShakeMapListFormatter.prototype.getListClassName = function () {
+		return 'shakemap-list';
 	};
 
+	ShakeMapListFormatter.prototype.generateListItemMarkup = function (item) {
+		var markup = [],
+		    prefix = this._idprefix,
+		    settings = this._options.settings,
+		    p, c, className, mmi, mmispan;
 
-	ShakeMapListFormatter.prototype = {
+		p = item.properties;
+		c = item.geometry.coordinates;
+		className = '';
 
-		initialize: function () {
-
-		},
-
-		generateListItemMarkup: function (items) {
-			var markup = [],
-			    prefix = this._idprefix,
-			    settings = this._options.settings,
-			    i, len, item, p, c, className, mmi;
-
-			if (items.length === 0) {
-				markup.push('<p class="nodata">No earthquakes to display</p>');
-			} else {
-				for (i = 0, len = items.length; i < len; i++) {
-					item = items[i];
-					p = item.properties;
-					c = item.geometry.coordinates;
-					className = '';
-
-					if (p.mmi !== null) {
-						mmi = Format.mmi(p.mmi);
-
-						if (p.mmi >= 600) {
-							className = ' class="bigger"';
-						} else if (p.mag >= 4.5) {
-							className = ' class="big"';
-						}
-
-						markup.push(
-						'<li id="', prefix, item.id, '"', className, '>',
-							'<span class="shakemap-intensity mmi',mmi,'">',
-								mmi,
-							'</span> ',
-							'<span class="place">',
-								this._generateListItemTitle(p.type, p.place),
-							'</span> ',
-							'<span class="time"> ',
-								Format.dateFromEvent(item, settings),
-							'</span> ',
-							'<span class="depth">',
-								Format.depth(c[2]),
-							' km</span>',
-						'</li>');
-					}
-				}
-			}
-			this._content.innerHTML = markup.join('');
+		if (p.mmi !== null) {
+			mmi = Format.mmi(p.mmi);
+			mmispan = 'intensity';
+		} else {
+			mmi = '&ndash;';
+			mmispan = 'no-shakemap';
 		}
 
+		if (p.mmi >= 600) {
+			className = ' class="bigger"';
+		} else if (p.mag >= 4.5) {
+			className = ' class="big"';
+		}
+
+		markup.push(
+		'<li id="', prefix, item.id, '"', className, '>',
+			'<span class="',mmispan,' mmi',mmi,'">',
+				mmi,
+			'</span> ',
+			'<span class="place">',
+				this._generateListItemTitle(p.mag, p.type, p.place),
+			'</span> ',
+			'<span class="time"> ',
+				Format.dateFromEvent(item, settings),
+			'</span> ',
+			'<span class="depth">',
+				Format.depth(c[2]),
+			' km</span>',
+		'</li>');
+
+		return markup.join('');
 	};
 
+	ShakeMapListFormatter.prototype._generateListItemTitle =
+			function (mag, type, place) {
+
+    if (mag !== null) {
+      mag = Format.magnitude(mag) + ' - ';
+    }
+
+    if (type === null || type.toLowerCase() === 'earthquake') {
+      return mag + place;
+    } else {
+      type = type.toLowerCase();
+      if (type === 'quarry') {
+        type = 'Quarry Blast';
+      } else if (type === 'nuke') {
+        type = 'Nuclear Explosion';
+      } else if (type === 'rockfall') {
+        type = 'Rockslide';
+      } else if (type === 'rockburst') {
+        type = 'Rockslide';
+      } else if (type === 'sonicboom') {
+        type = 'Sonic Boom';
+      } else {
+        type = type.replace(/\w\S*/g,
+            function (txt) {
+              return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+            }
+        );
+      }
+    }
+    return mag + type + ' ' + place;
+
+	};
 
 	return ShakeMapListFormatter;
 });
