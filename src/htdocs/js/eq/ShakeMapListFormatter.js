c076be67e4682099a572215aac638ac74f839133
Hal Simpson <hasimpson@usgs.gov>
2014-11-05 10:51:25 -0700

    updated per changes suggested in LATESTEQ-251

--- src/htdocs/js/eq/ShakeMapListFormatter.js
+++ src/htdocs/js/eq/ShakeMapListFormatter.js
@@ -1,102 +1,74 @@
 /* global define */
 define ([
-	'./DefaultListFormatter',
-	'mvc/Util',
-	'./Format'
+  './DefaultListFormatter',
+  'mvc/Util',
+  './Format'
 ], function(
-	DefaultListFormatter,
-	Util,
-	Format
+  DefaultListFormatter,
+  Util,
+  Format
 ){
-	'use strict';
+  'use strict';
 
-	var ShakeMapListFormatter = function (/*options*/) {
-		DefaultListFormatter.apply(this, arguments);
-	};
+  var DEFAULTS = {
+    className: 'shakemap-list'
+  };
 
-	ShakeMapListFormatter.prototype =
-			Object.create(DefaultListFormatter.prototype);
+  var ShakeMapListFormatter = function (options) {
+    DefaultListFormatter.apply(this, arguments);
+    this._options = Util.extend({},DEFAULTS, options);
+  };
+
+  ShakeMapListFormatter.prototype =
+      Object.create(DefaultListFormatter.prototype);
 
 
-	ShakeMapListFormatter.prototype.getListClassName = function () {
-		return 'shakemap-list';
-	};
+  ShakeMapListFormatter.prototype.getListClassName = function () {
+    return this._options.className;
+  };
 
-	ShakeMapListFormatter.prototype.generateListItemMarkup = function (item) {
-		var markup = [],
-		    prefix = this._idprefix,
-		    settings = this._options.settings,
-		    p, c, className, mmi, mmispan;
+  ShakeMapListFormatter.prototype.generateListItemMarkup = function (item) {
+    var markup = [],
+        prefix = this._idprefix,
+        settings = this._options.settings,
+        p, c, highlightClass, mmi, mmispan;
 
-		p = item.properties;
-		c = item.geometry.coordinates;
-		className = '';
+    p = item.properties;
+    c = item.geometry.coordinates;
+    highlightClass = '';
 
-		if (p.mmi !== null) {
-			mmi = Format.mmi(p.mmi);
-			mmispan = 'intensity';
-		} else {
-			mmi = '&ndash;';
-			mmispan = 'no-shakemap';
-		}
-
-		if (p.mmi >= 600) {
-			className = ' class="bigger"';
-		} else if (p.mag >= 4.5) {
-			className = ' class="big"';
-		}
-
-		markup.push(
-		'<li id="', prefix, item.id, '"', className, '>',
-			'<span class="',mmispan,' mmi',mmi,'">',
-				mmi,
-			'</span> ',
-			'<span class="place">',
-				this._generateListItemTitle(p.mag, p.type, p.place),
-			'</span> ',
-			'<span class="time"> ',
-				Format.dateFromEvent(item, settings),
-			'</span> ',
-			'<span class="depth">',
-				Format.depth(c[2]),
-			' km</span>',
-		'</li>');
-
-		return markup.join('');
-	};
-
-	ShakeMapListFormatter.prototype._generateListItemTitle =
-			function (mag, type, place) {
-
-    if (mag !== null) {
-      mag = Format.magnitude(mag) + ' - ';
-    }
-
-    if (type === null || type.toLowerCase() === 'earthquake') {
-      return mag + place;
+    if (p.mmi !== null) {
+      mmi = Format.mmi(p.mmi);
+      mmispan = 'intensity';
     } else {
-      type = type.toLowerCase();
-      if (type === 'quarry') {
-        type = 'Quarry Blast';
-      } else if (type === 'nuke') {
-        type = 'Nuclear Explosion';
-      } else if (type === 'rockfall') {
-        type = 'Rockslide';
-      } else if (type === 'rockburst') {
-        type = 'Rockslide';
-      } else if (type === 'sonicboom') {
-        type = 'Sonic Boom';
-      } else {
-        type = type.replace(/\w\S*/g,
-            function (txt) {
-              return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-            }
-        );
-      }
+      mmi = '&ndash;';
+      mmispan = 'no-shakemap';
     }
-    return mag + type + ' ' + place;
 
-	};
+    if (p.mmi >= 600) {
+      highlightClass = ' class="bigger"';
+    } else if (p.mag >= 4.5) {
+      highlightClass = ' class="big"';
+    }
 
-	return ShakeMapListFormatter;
+    markup.push(
+    '<li id="', prefix, item.id, '"', highlightClass, '>',
+      '<span class="',mmispan,' mmi',mmi,'">',
+        mmi,
+      '</span> ',
+      '<span class="place">',
+        p.title,
+      '</span> ',
+      '<span class="time"> ',
+        Format.dateFromEvent(item, settings),
+      '</span> ',
+      '<span class="depth">',
+        Format.depth(c[2]),
+      ' km</span>',
+    '</li>');
+
+    return markup.join('');
+  };
+
+  return ShakeMapListFormatter;
 });
