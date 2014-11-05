13e4581de6ca61bc5b1bf1f917c5392209cf4737
Eddie McWhirter <emcwhirter@usgs.gov>
2014-11-05 10:17:37 -0700

    Clean up large if statement, use title property for the title.

--- src/htdocs/js/eq/DYFIListFormatter.js
+++ src/htdocs/js/eq/DYFIListFormatter.js
@@ -22,92 +22,50 @@ define([
 	DYFIListFormatter.prototype.generateListItemMarkup = function(item) {
 		var prefix = this._options.idprefix,
 		    settings = this._options.settings,
-		    p,
-		    cdi,
-		    className,
-		    plural;
+		    p = item.properties,
+		    cdi = Format.mmi(p.cdi),
+		    className = '',
+		    felt = p.felt,
+		    mmiSpan,
+		    responses;
 
-		p = item.properties;
+		if (felt !== null && felt !== 0) {
+			mmiSpan = '<span class="intensity mmi' + cdi + '">';
 
-		if (p.felt !== null && p.felt !== 0) {
-			cdi = Format.mmi(p.cdi);
-			className = '';
-			plural = '';
-
-			if (p.felt > 1) {
-				plural = 's';
+			if (felt != 1) {
+				responses = ' responses'
+			} else if (felt == 1) {
+				responses = ' response';
 			}
-
-			if (p.sig >= 600) {
-				className = ' class="bigger"';
-			} else if (p.mag >= 4.5) {
-				className = ' class="big"';
-			}
-
-			return [
-				'<li id="', prefix, item.id, '"', className, '>',
-					'<span class="intensity mmi', cdi, '">',
-						cdi,
-					'</span> ',
-					'<span class="place">',
-						this._generateListItemTitle(p.mag, p.type, p.place),
-					'</span> ',
-					'<span class="time"> ',
-						Format.dateFromEvent(item, settings),
-					'</span> ',
-					'<span class="responses">',
-						p.felt,
-					' response', plural, '</span>',
-				'</li>'
-			].join('');
 		} else {
-			return [
-				'<li id="', prefix, item.id, '"', className, '>',
-					'<span class="no-dyfi">',
-						'&ndash;',
-					'</span> ',
-					'<span class="place">',
-						this._generateListItemTitle(p.mag, p.type, p.place),
-					'</span> ',
-					'<span class="time"> ',
-						Format.dateFromEvent(item, settings),
-					'</span> ',
-					'<span class="responses">',
-						p.felt,
-					' response', plural, '</span>',
-				'</li>'
-			].join('');
+			cdi = '&ndash;';
+			felt = '';
+			mmiSpan = '<span class="no-dyfi">';
+			responses = '&ndash;';
 		}
-	};
 
-	DYFIListFormatter.prototype._generateListItemTitle =
-			function (mag, type, place) {
-
-		mag = 'M ' + Format.magnitude(mag) + ' - ';
-
-		if (type === null || type.toLowerCase() === 'earthquake') {
-			return mag + place;
-		} else {
-			type = type.toLowerCase();
-			if (type === 'quarry') {
-				type = 'Quarry Blast';
-			} else if (type === 'nuke') {
-				type = 'Nuclear Explosion';
-			} else if (type === 'rockfall') {
-				type = 'Rockslide';
-			} else if (type === 'rockburst') {
-				type = 'Rockslide';
-			} else if (type === 'sonicboom') {
-				type = 'Sonic Boom';
-			} else {
-				type = type.replace(/\w\S*/g,
-						function (txt) {
-							return mag + txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-						}
-				);
-			}
+		if (p.sig >= 600) {
+			className = ' class="bigger"';
+		} else if (p.mag >= 4.5) {
+			className = ' class="big"';
 		}
-		return mag + type + ' ' + place;
+		console.log(p);
+		return [
+			'<li id="', prefix, item.id, '"', className, '>',
+				mmiSpan,
+					cdi,
+				'</span> ',
+				'<span class="place">',
+					p.title,
+				'</span> ',
+				'<span class="time"> ',
+					Format.dateFromEvent(item, settings),
+				'</span> ',
+				'<span class="responses">',
+					felt,
+				responses, '</span>',
+			'</li>'
+		].join('');
 	};
 
 	return DYFIListFormatter;
