be89955120641f05ab6dc6e3fbc5b022d045245b
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2015-03-09 08:58:26 -0600

    updated text in navigation

--- src/htdocs/_navigation.inc.php
+++ src/htdocs/_navigation.inc.php
@@ -1,7 +1,6 @@
 <?php
 
 echo  	navItem('./', 'Latest Earthquakes') .
-	navItem('./doc_aboutdata.php', 'About ANSS Comprehensive Catalog') .
-	navItem('./doc_help.php', 'Using the Latest Earthquakes Map and List') . 
+	navItem('./doc_aboutdata.php', 'About the ANSS Comprehensive Catalog and Important Caveats') .
+	navItem('./doc_help.php', 'Using the Latest Earthquakes Map and List') .
 	navItem('./doc_whicheqs.php', 'Which Earthquakes are Included on the Map and List');
-	
