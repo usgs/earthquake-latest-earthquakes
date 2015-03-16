d32f9db8929ef58defb77b940a0212481d62717e
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2015-03-16 13:48:58 -0600

    replaced tabs with spaces

--- src/htdocs/doc_whicheqs.php
+++ src/htdocs/doc_whicheqs.php
@@ -1,71 +1,71 @@
 <?php
 
-	if (!isset($TEMPLATE)) {
-		$TITLE = 'Which Earthquakes are Included on the Map and List';
-		$NAVIGATION = true;
+  if (!isset($TEMPLATE)) {
+    $TITLE = 'Which Earthquakes are Included on the Map and List';
+    $NAVIGATION = true;
 
-		include 'template.inc.php';
-	}
+    include 'template.inc.php';
+  }
 ?>
 
 <header>
-	<ul class="topics">
-		<li>Why are there &ldquo;missing&rdquo; earthquakes?</li>
-		<li>There is an earthquake on another website; why is it not here?</li>
-	</ul>
+  <ul class="topics">
+    <li>Why are there &ldquo;missing&rdquo; earthquakes?</li>
+    <li>There is an earthquake on another website; why is it not here?</li>
+  </ul>
 </header>
 
 <p>
-	The maps and lists show events which have been located by the USGS and
-	contributing agencies within the last 30 days. They should not be considered
-	to be complete lists of all events in the US and adjacent areas and
-	especially should not be considered to be complete lists of all events M4.5+
-	in the world.
+  The maps and lists show events which have been located by the USGS and
+  contributing agencies within the last 30 days. They should not be considered
+  to be complete lists of all events in the US and adjacent areas and
+  especially should not be considered to be complete lists of all events M4.5+
+  in the world.
 </p>
 
 <p>
-	In most cases, we locate and report on earthquakes worldwide of magnitude 5.0
-	and larger in 30 minutes or less. Additionally, we locate and report on
-	earthquakes magnitude 4.0 and larger within the contiguous US and populated
-	regions of Alaska within 30 minutes. Earthquakes of magnitude 2.0 or greater
-	within the US and populated regions of Alaska are often rapidly reported if
-	they occur within the <a href="/monitoring/anss/">region of a contributing
-	local seismic network</a>.
+  In most cases, we locate and report on earthquakes worldwide of magnitude 5.0
+  and larger in 30 minutes or less. Additionally, we locate and report on
+  earthquakes magnitude 4.0 and larger within the contiguous US and populated
+  regions of Alaska within 30 minutes. Earthquakes of magnitude 2.0 or greater
+  within the US and populated regions of Alaska are often rapidly reported if
+  they occur within the <a href="/monitoring/anss/">region of a contributing
+  local seismic network</a>.
 </p>
 
 <p>
-	We may not rapidly locate earthquakes smaller than 5.0 outside the US unless
-	they have caused significant damage or are widely felt. Earthquakes this small
-	rarely cause significant damage. At times, some other agency may report an
-	earthquake with a larger magnitude than what we compute from our data,
-	especially for non-US events near magnitude 5.0. If our magnitude for the
-	event is less than magnitude 5.0, we may not issue a rapid report for it.
+  We may not rapidly locate earthquakes smaller than 5.0 outside the US unless
+  they have caused significant damage or are widely felt. Earthquakes this small
+  rarely cause significant damage. At times, some other agency may report an
+  earthquake with a larger magnitude than what we compute from our data,
+  especially for non-US events near magnitude 5.0. If our magnitude for the
+  event is less than magnitude 5.0, we may not issue a rapid report for it.
 </p>
 
 <p>
-	Earthquakes occurring outside the US and smaller than about magnitude 4.5 can
-	be difficult for the USGS to locate if there are not enough data. The USGS
-	continues to receive data from observatories throughout the world for several
-	months after the events occur. Using those data, we add new events and revise
-	existing events in later publications. For a description of these later
-	publications and the data available, see <a href="/data/">Scientific Data</a>.
+  Earthquakes occurring outside the US and smaller than about magnitude 4.5 can
+  be difficult for the USGS to locate if there are not enough data. The USGS
+  continues to receive data from observatories throughout the world for several
+  months after the events occur. Using those data, we add new events and revise
+  existing events in later publications. For a description of these later
+  publications and the data available, see <a href="/data/">Scientific Data</a>.
 </p>
 
 <p>
-	There are many regional networks around the world that can record smaller
-	earthquakes in their region than the NEIC global network can, and in many
-	cases these regional networks do not share their data with the NEIC.  So if
-	you think there is a missing earthquake on our maps and lists, please see the
-	national and regional links for the area of interest on these webpages:
+  There are many regional networks around the world that can record smaller
+  earthquakes in their region than the NEIC global network can, and in many
+  cases these regional networks do not share their data with the NEIC.  So if
+  you think there is a missing earthquake on our maps and lists, please see the
+  national and regional links for the area of interest on these webpages:
 </p>
 
 <ul>
-	<li>
-		<a href="/earthquakes/states/">Earthquake Info by State/Territory</a>
-	</li>
+  <li>
+    <a href="/earthquakes/states/">Earthquake Info by State/Territory</a>
+  </li>
 </ul>
 
 <p>
-	Also see: <a href="http://www.usgs.gov/faq/taxonomy/term/9826">FAQs about
-			real-time earthquake information</a>
+  Also see: <a href="http://www.usgs.gov/faq/taxonomy/term/9826">FAQs about
+      real-time earthquake information</a>
 </p>
