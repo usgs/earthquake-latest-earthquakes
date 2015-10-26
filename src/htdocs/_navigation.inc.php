b8fb4a57d8a265ee34ad105d822c6f9e88702d2e
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2015-10-26 07:04:38 -0600

    changed navigation back to the old setup

--- src/htdocs/_navigation.inc.php
+++ src/htdocs/_navigation.inc.php
@@ -1,9 +1,6 @@
 <?php
-
-echo navGroup(navItem('./', 'Latest Earthquakes'),
+  navItem('./', 'Latest Earthquakes') .
   navItem('./doc_aboutdata.php','About the ANSS Comprehensive Catalog and Important Caveats') .
   navItem('./doc_help.php', 'Using the Latest Earthquakes Map and List') .
-  navItem('./doc_whicheqs.php','Which Earthquakes are Included on the Map and List')
-);
-
+  navItem('./doc_whicheqs.php','Which Earthquakes are Included on the Map and List');
 ?>
