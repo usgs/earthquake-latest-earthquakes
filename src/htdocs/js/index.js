08625b69651a866fcb8156376893336fd59831d0
Eric Martinez <emartinez@usgs.gov>
2015-10-29 10:37:45 -0600

    Simplified runtime config for requirejs.

--- src/htdocs/js/index.js
+++ src/htdocs/js/index.js
@@ -1,15 +1,6 @@
 require.config({
   baseUrl: 'js',
-  paths: {
-    leaflet: '../leaflet/dist/leaflet-custom-src',
-    localStorage: '../lib/localStorage',
-    JSON: '../lib/JSON'
-  },
-  shim: {
-    leaflet: {exports: 'L'},
-    localStorage: {exports: 'localStorage'},
-    JSON: {exports: 'JSON'}
-  }
+  urlArgs: 'stamp=' + (new Date()).getTime()
 });
 
 
