bddb98eeb9d4aeaa225efd15173422ca3b3faf05
Eric Martinez <emartinez@usgs.gov>
2015-10-29 13:14:39 -0600

    Removed unnecessary ./ from links.

--- src/htdocs/_navigation.inc.php
+++ src/htdocs/_navigation.inc.php
@@ -1,6 +1,9 @@
 <?php
 echo navItem('./', 'Latest Earthquakes') .
-  navItem('./doc_aboutdata.php','About the ANSS Comprehensive Catalog and Important Caveats') .
-  navItem('./doc_help.php', 'Using the Latest Earthquakes Map and List') .
-  navItem('./doc_whicheqs.php','Which Earthquakes are Included on the Map and List');
+  navItem('doc_aboutdata.php',
+      'About the ANSS Comprehensive Catalog and Important Caveats') .
+  navItem('doc_help.php',
+      'Using the Latest Earthquakes Map and List') .
+  navItem('doc_whicheqs.php',
+      'Which Earthquakes are Included on the Map and List');
 ?>
