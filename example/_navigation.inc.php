fe2f155ea23f235f5a2e84bc4820d0b604539dba
Hal Simpson <hasimpson@usgs.gov>
2016-04-13 14:22:10 -0600

    Added ShakeMapListFormat to example nav

--- example/_navigation.inc.php
+++ example/_navigation.inc.php
@@ -5,7 +5,8 @@
 
   echo navGroup('List',
     navItem('/list/DefaultListFormatExample.php', 'DefaultListFormat') .
-    navItem('/list/PagerListFormatExample.php', 'PagerListFormat')
+    navItem('/list/PagerListFormatExample.php', 'PagerListFormat') .
+    navItem('/list/ShakeMapListFormatExample.php', 'ShakeMapListFormat')
   );
 
 ?>
