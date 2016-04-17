102be1bef1d0df29098d68adf2fbec00fb86a566
Hal Simpson <hasimpson@usgs.gov>
2016-04-17 16:22:23 -0600

    Added overrides for getClasses, getCallupMarkup,  getAsideMarkup and getHeaderMarkup

--- src/htdocs/js/list/DyfiListFormat.js
+++ src/htdocs/js/list/DyfiListFormat.js
@@ -21,6 +21,66 @@ var DyfiListFormat = function (options) {
     _formatter = options.formatter || Formatter();
   };
 
+  _this.getAsideMarkup = function (eq) {
+    var felt,
+        responses;
+
+    felt = _this.getProperty(eq, 'felt');
+
+    if ( felt !== null) {
+      if (felt !== 1) {
+        responses = felt + ' responses';
+      } else {
+        responses = felt + ' responses';
+      }
+    } else {
+      responses = '&ndash; responses';
+    }
+
+    return '<span class="responses">' + responses + '</span>';
+  };
+
+
+  _this.getCalloutMarkup = function (eq) {
+    var cdi,
+        felt,
+        mmiClass;
+
+    felt = _this.getProperty(eq,'felt');
+
+    if (felt === null) {
+      cdi = '&ndash;';
+      mmiClass = 'no-dyfi';
+    } else {
+      cdi = _this.getProperty(eq,'cdi');
+      cdi = _formatter.mmi(cdi);
+      mmiClass = 'roman mmi mmi' + cdi;
+    }
+
+    return '<span class="' + mmiClass + '">' + cdi + '</span>';
+  };
+
+  _this.getClasses = Util.compose(_this.getClasses, function (params) {
+    params = params || {};
+    params.classes = params.classes || [];
+
+    params.classes.push('dyfi-list-item');
+
+    return params;
+  });
+
+  /**
+   * APIMethod.
+   *
+   * Uses the "place" property to display the event header value.
+   *
+   * @see DefaultListFormat#getHeaderMarkup
+   */
+  _this.getHeaderMarkup = function (eq) {
+    return _this.getProperty(eq, 'title');
+  };
+
+
   _initialize(options);
   options = null;
   return _this;
