5c81434327e0f660cb8f7bbe9ab9e99a8233553f
Hal Simpson <hasimpson@usgs.gov>
2016-04-17 16:29:41 -0600

    Added DyfiListFormat

--- example/_navigation.inc.php
+++ example/_navigation.inc.php
@@ -5,6 +5,7 @@
 
   echo navGroup('List',
     navItem('/list/DefaultListFormatExample.php', 'DefaultListFormat') .
+        navItem('/list/DyfiListFormatExample.php', 'DyfiListFormat') .
     navItem('/list/PagerListFormatExample.php', 'PagerListFormat') .
     navItem('/list/ShakeMapListFormatExample.php', 'ShakeMapListFormat')
   );
