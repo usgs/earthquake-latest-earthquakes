d1bf6d5640c88c9571da43cd2052201d91a0e03d
Hal Simpson <hasimpson@usgs.gov>
2016-04-18 13:33:15 -0600

    Updated tests to include getClasses, and getHeaderMarkup, and refactored to be more complete

--- test/spec/list/ShakeMapListFormatTest.js
+++ test/spec/list/ShakeMapListFormatTest.js
@@ -1,4 +1,4 @@
-/* global chai, describe, it */
+/* global afterEach, beforeEach, chai, describe, it */
 'use strict';
 
 
@@ -9,6 +9,21 @@ var expect = chai.expect;
 
 
 describe('ShakeMapListFormat', function () {
+  var shakeMapListFormat;
+
+  afterEach(function () {
+    try {
+      shakeMapListFormat.destroy();
+    } catch (e) {
+      /* Ignore, probably just already destroyed */
+    }
+  });
+
+  beforeEach(function () {
+    shakeMapListFormat = ShakeMapListFormat();
+  });
+
+
   describe('constructor', function () {
     it('is defined', function () {
       expect(typeof ShakeMapListFormat).to.equal('function');
@@ -20,13 +35,37 @@ describe('ShakeMapListFormat', function () {
   });
 
   describe('getCalloutMarkup', function () {
-    var shakeMapListFormat = ShakeMapListFormat();
     it('returns shakemap specific markup', function () {
       expect(shakeMapListFormat.getCalloutMarkup({
         properties: {
           mmi:4.4
         }
-      })).to.contain('mmiIV');
+      })).to.equal('<span class="roman mmi mmiIV">IV</span>');
     });
   });
+
+  describe('getClasses', function () {
+    it('contains the shakemap class', function () {
+      var result;
+
+      result = shakeMapListFormat.getClasses();
+      expect(result.classes.indexOf('shakemap-list-item')).to.not.equal(-1);
+    });
+  });
+
+  describe('getHeaderMarkup', function () {
+    it('gets correct property', function () {
+      var result;
+
+      result = shakeMapListFormat.getHeaderMarkup({
+        properties: {
+          title:'M 4.1 - 9km NE of Noatak, Alaska'
+        }
+      });
+
+      expect(result).to.equal('M 4.1 - 9km NE of Noatak, Alaska');
+
+    });
+  });
+
 });
