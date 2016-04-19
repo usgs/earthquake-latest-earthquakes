9d917bf2a0b282901d1f301538c5d9c2790ab4c9
Eric Martinez <emartinez@usgs.gov>
2016-04-19 09:10:05 -0600

    Made example more complete/interactive.

--- example/list/ListViewExample.js
+++ example/list/ListViewExample.js
@@ -1,22 +1,41 @@
 'use strict';
 
 var Collection = require('mvc/Collection'),
+    DefaultListFormat = require('list/DefaultListFormat'),
+    DYFIListFormat = require('list/DefaultListFormat'), // TODO
     ListView = require('list/ListView'),
+    PagerListFormat = require('list/PagerListFormat'),
+    ShakeMapListFormat = require('list/ShakeMapListFormat'),
     Xhr = require('util/Xhr');
 
 
 var collection,
-    listView;
+    formats,
+    listView,
 
+    onFormatClick;
+
+
+formats = {
+  'default-format': null,
+  'magnitude-format': DefaultListFormat(),
+  'dyfi-format': DYFIListFormat(),
+  'shakemap-format': ShakeMapListFormat(),
+  'pager-format': PagerListFormat()
+};
+
+// -- Start basic usage example
 
 collection = Collection();
 
 listView = ListView({
-  // listFormat: require('list/DefaultListFormat')(), // optional
-
   catalog: collection, // may be latesteqs/Catalog, but only need Collection
   el: document.querySelector('#list-view-example')
 });
+
+// -- End basic usage example
+
+
 
 
 Xhr.ajax({
@@ -28,3 +47,31 @@ Xhr.ajax({
     collection.reset([]);
   }
 });
+
+onFormatClick = function (evt) {
+  var format,
+      target;
+
+  target = evt ? evt.target : null;
+
+  if (target && target.nodeName.toUpperCase() === 'BUTTON') {
+    Array.prototype.forEach.call(evt.currentTarget.querySelectorAll('button'),
+    function (button) {
+      if (button === target) {
+        button.classList.add('green');
+        button.classList.add('confirm');
+      } else {
+        button.classList.remove('green');
+        button.classList.remove('confirm');
+      }
+    });
+
+    format = target.getAttribute('data-format');
+    if (formats.hasOwnProperty(format)) {
+      listView.model.set({'listFormat': formats[format]});
+    }
+  }
+};
+
+document.querySelector('.format-options').addEventListener('click',
+    onFormatClick);
--- example/list/ListViewExample.php
+++ example/list/ListViewExample.php
@@ -10,6 +10,9 @@ if (!isset($TEMPLATE)) {
     #list-view-example {
       border: 1px solid #666;
     }
+    .format-options > li {
+      display: inline-block;
+    }
     </style>
   ';
 
@@ -22,4 +25,29 @@ if (!isset($TEMPLATE)) {
 }
 ?>
 
+<p class="alert">
+  The view itself appears in the visualization section below. The visualization
+  includes borders between each list item and between the list
+  header/content/footer, but the border around the entire view is added by the
+  example in order to reflect what this view might look like within the
+  application itself. Such borders (if desired) are expected to appear as part
+  of the application scaffold.
+</p>
+
+<h2>Available Formats</h2>
+<p>
+  Click a format below to change how the list renders. Changes occur by
+  setting the model property &ldquo;listFormat&rdquo; on the listView.
+</p>
+<ul class="no-style format-options">
+  <li>
+    <button class="confirm green" data-format="default-format">JSON</button>
+  </li>
+  <li><button data-format="magnitude-format">Magnitude</button></li>
+  <li><button data-format="dyfi-format">DYFI (TODO)</button></li>
+  <li><button data-format="shakemap-format">ShakeMap</button></li>
+  <li><button data-format="pager-format">PAGER</button></li>
+</ul>
+
+<h2>Visualization</h2>
 <div id="list-view-example"></div>
