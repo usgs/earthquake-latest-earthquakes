15079911b1cb67c92b8a306c2480d8a9481e5771
Eric Martinez <emartinez@usgs.gov>
2016-04-19 15:53:19 -0600

    Added comments.

--- src/htdocs/js/core/GenericCollectionView.js
+++ src/htdocs/js/core/GenericCollectionView.js
@@ -15,6 +15,40 @@ var _DEFAULTS = {
 };
 
 
+/**
+ * Base class to serve as an extension point for multiple views used in
+ * the usgs/earthquake-latest-earthquakes project. This view accepts (among
+ * other things) a collection and a model on the configuration options at
+ * time of construction.
+ *
+ * The collection provides a list of all items for the collection view to
+ * render. The view listens to "add", "remove", and "reset" events on the
+ * provided collection and calls the render method when such events occur.
+ * The view does not listen to select/deselect on the collection...
+ *
+ * The model provides information regarding the currently selected values
+ * for various properties. By default, this view only listens for changes to
+ * the configured `options.watchProperty` property, and when that property
+ * changes, the view calls its `onEvent` method.
+ *
+ * All messaging to/from this view should occur view events/method calls on
+ * the provided model.
+ *
+ * Implementing sub-classes may override any public method for customized
+ * behavior, however, typical extension points may involve overriding the
+ * following methods:
+ *
+ * - createCollectionItemContent
+ * - deselectAll
+ * - renderFooter
+ * - renderHeader
+ * - setSelected
+ *
+ * @param options {Object}
+ *     Configuration options for this view. See documentation on the
+ *     `_initialize` method for complete details on all configuration
+ *     options that may be provided.
+ */
 var GenericCollectionView = function (options) {
   var _this,
       _initialize,
@@ -29,6 +63,30 @@ var GenericCollectionView = function (options) {
   options = Util.extend({}, _DEFAULTS, options);
   _this = View(options);
 
+  /**
+   * Constructor.
+   *
+   * @param options {Object}
+   *     Configuration options for this view.
+   * @param options.classPrefix {String}
+   *     A prefix to add to each class that is added to generated HTMLElements
+   *     in this view's default implementation.
+   * @param options.collection {mvc/Collection}
+   *     The collection for this view.
+   * @param options.containerNodeName {String}
+   *     The nodeName of the element to be created that will wrap all
+   *     the items in the collection. For example: 'ul'
+   * @param options.itemNodeName {String}
+   *     The nodeName of the element to be created that will wrap each of
+   *     the items in the collection. For example: 'li'
+   * @param options.model {mvc/Model}
+   *     The model for this view.
+   * @param options.noDataMessage {String}
+   *     A message to display when the collection is empty
+   * @param options.watchProperty {String}
+   *     The name of the property on the model to watch for changes and
+   *     subsequently trigger `onEvent`.
+   */
   _initialize = function (options) {
     _this.collection = options.collection || Collection();
 
@@ -51,6 +109,14 @@ var GenericCollectionView = function (options) {
   };
 
 
+  /**
+   * Creates the container element by which all the items in the collection
+   * are wraped.
+   *
+   * @return {HTMLElement}
+   *     An HTMLElement based on the configured `options.containerNodeName`
+   *     property.
+   */
   _this.createCollectionContainer = function () {
     var container;
 
@@ -60,6 +126,16 @@ var GenericCollectionView = function (options) {
     return container;
   };
 
+  /**
+   * Creates and populates an element for the individual given `obj`.
+   *
+   * @param obj {Object}
+   *     The item from the collection for which to create the element.
+   *
+   * @return {HTMLElement}
+   *     An HTMLElement based on the configured `options.itemNodeName`
+   *     property.
+   */
   _this.createCollectionItem = function (obj) {
     var item;
 
@@ -74,6 +150,18 @@ var GenericCollectionView = function (options) {
     return item;
   };
 
+  /**
+   * APIMethod
+   *
+   * Creates the content for the given obj. Implementing classes will
+   * likely want to override this method.
+   *
+   * @param obj {Object}
+   *     The item from the collection for which to create the content.
+   *
+   * @return {HTMLElement}
+   *     The content visualization for the given `obj`.
+   */
   _this.createCollectionItemContent = function (obj) {
     var pre;
 
@@ -83,6 +171,12 @@ var GenericCollectionView = function (options) {
     return pre;
   };
 
+  /**
+   * Called during initialization. Creates the base HTML structure into
+   * which all rendering is later performed. Sets up event delegation on
+   * `_this.content` to handle click events.
+   *
+   */
   _this.createScaffold = function () {
     var el;
 
@@ -101,6 +195,14 @@ var GenericCollectionView = function (options) {
     _this.content.addEventListener('click', _this.onContentClick, _this);
   };
 
+  /**
+   * APIMethod
+   *
+   * Deselects all the items in `_this.content`. An implementing sub-class
+   * may want to override this method if selection is not done by toggling
+   * a "selected" class on the item element.
+   *
+   */
   _this.deselectAll = function () {
     Array.prototype.forEach.call(_this.content.querySelectorAll('.selected'),
     function (node) {
@@ -110,6 +212,10 @@ var GenericCollectionView = function (options) {
     });
   };
 
+  /**
+   * Frees resources associated with this view.
+   *
+   */
   _this.destroy = Util.compose(function () {
     _this.content.removeEventListener('click', _this.onContentClick, _this);
 
@@ -132,6 +238,22 @@ var GenericCollectionView = function (options) {
     _this = null;
   }, _this.destroy);
 
+  /**
+   * Traverse a DOM from the startNode to the endNode looking for the clicked
+   * item node that wraps the individual item from the collection.
+   *
+   * Note: An implementing sub-class may want to override this method if
+   *       it also significantly changes the `createCollectionItem` method.
+   *
+   * @param startNode {HTMLElement}
+   *     The starting point for the search.
+   * @param endNode {HTMLElement}
+   *     The ending point for the search.
+   *
+   * @return {HTMLElement|null}
+   *     The element corresponding to the clicked item, or null if no such
+   *     element was found.
+   */
   _this.getClickedItem = function (startNode, endNode) {
     var item;
 
@@ -144,6 +266,16 @@ var GenericCollectionView = function (options) {
     return item;
   };
 
+  /**
+   * Called via event delegation when the user clicks anywhere withing
+   * `_this.content`. Finds the clicked element and uses its "data-id"
+   * attribute to find the corresponding item in the collection and then
+   * selects the `_watchProperty` on `_this.model` to that item.
+   *
+   *
+   * @param evt {ClickEvent}
+   *     The event the caused this method to be called.
+   */
   _this.onContentClick = function (evt) {
     var item,
         toSet;
@@ -153,13 +285,19 @@ var GenericCollectionView = function (options) {
 
       if (item && _watchProperty) {
         toSet = {};
-        toSet[_watchProperty] = _this.collection.get(item.getAttribute('data-id'));
+        toSet[_watchProperty] =
+            _this.collection.get(item.getAttribute('data-id'));
 
         _this.model.set(toSet);
       }
     }
   };
 
+  /**
+   * Called when the `_watchProperty` on `_this.model` is changed. Ensures
+   * the proper corresponding rendered element(s) appear selected.
+   *
+   */
   _this.onEvent = function () {
     var value;
 
@@ -170,12 +308,25 @@ var GenericCollectionView = function (options) {
     }
   };
 
+  /**
+   * Called when the collection is reset (primarily), or when items are added
+   * to/removed from the collection. Renders the header, footer, and all
+   * the content.
+   *
+   */
   _this.render = function () {
     _this.renderHeader();
     _this.renderContent();
     _this.renderFooter();
   };
 
+  /**
+   * Renders the content section for this view. If no data exists, renders
+   * a the `_noDataMessage`. If an error occurs, renders a generic error
+   * message. Otherwise, renders each item in the collection, delegating to
+   * sub-methods.
+   *
+   */
   _this.renderContent = function () {
     var container,
         data;
@@ -206,14 +357,37 @@ var GenericCollectionView = function (options) {
     }
   };
 
+  /**
+   * APIMethod
+   *
+   * A hook allowing markup to be added to `_this.footer` section.
+   */
   _this.renderFooter = function () {
     _this.footer.innerHTML = ''; // By default, no content
   };
 
+  /**
+   * APIMethod
+   *
+   * A hook allowing markup to be added to `_this.header` section.
+   */
   _this.renderHeader = function () {
     _this.header.innerHTML = ''; // By default, no content
   };
 
+  /**
+   * APIMethod
+   *
+   * Method to update an element in `_this.content`, whose "data-id" attribute
+   * matches the given value "id" attribute, to appear selected. An
+   * implementing sub-class may want to override this method if selection is
+   * not done by toggling a "selected" class on the item element.
+   *
+   * @param value {Mixed}
+   *     Typically an object with an "id" attribute which corresponds the
+   *     "data-id" attribute of some element in `_this.content`.
+   *
+   */
   _this.setSelected = function (value) {
     var item;
 
