9f9004bfb79872b4e825c1b4e6f4e68208a33a64
ehunter-usgs <ehunter@usgs.gov>
2016-04-20 12:45:44 -0600

    Add comments and update code to be more precise

--- src/htdocs/js/settings/CheckboxOptionsView.js
+++ src/htdocs/js/settings/CheckboxOptionsView.js
@@ -5,7 +5,36 @@ var GenericCollectionView = require('core/GenericCollectionView'),
 
 var _DEFAULTS = {};
 
-
+/**
+ * Serves as a resuable view to display configuration options. This view
+ * accepts (among other things) a collection and a model on the configuration
+ * options at time of construction.
+ *
+ * The collection provides a list of all items for the collection view to
+ * render. The view listens to "add", "remove", and "reset" events on the
+ * provided collection and calls the render method when such events occur.
+ * The view does not listen to select/deselect on the collection...
+ *
+ * The model provides information regarding the currently selected values
+ * for various properties. By default, this view only listens for changes to
+ * the configured `options.watchProperty` property, and when that property
+ * changes, the view calls its `onEvent` method.
+ *
+ * All messaging to/from this view should occur view events/method calls on
+ * the provided model.
+ *
+ * Implementing sub-classes may override any public method for customized
+ * behavior, however, typical extension points may involve overriding the
+ * following methods:
+ *
+ * - deselectAll
+ * - setSelected
+ *
+ * @param options {Object}
+ *     Configuration options for this view. See documentation on the
+ *     `_initialize` method for complete details on all configuration
+ *     options that may be provided.
+ */
 var RadioOptionsView = function (options) {
   var _this,
       _initialize,
@@ -16,6 +45,20 @@ var RadioOptionsView = function (options) {
   _this = GenericCollectionView(options);
   options = Util.extend({}, _DEFAULTS, options);
 
+
+  /**
+   * Constructor.
+   *
+   * @param options {Object}
+   *     Configuration options for this view.
+   * @param options.collection {mvc/Collection}
+   *     The collection for this view.
+   * @param options.model {mvc/Model}
+   *     The model for this view.
+   * @param options.watchProperty {String}
+   *     The name of the property on the model to watch for changes and
+   *     subsequently trigger `onEvent`.
+   */
   _initialize = function (options) {
     // defines which setting to configure
     _watchProperty = options.watchProperty || '';
@@ -23,15 +66,25 @@ var RadioOptionsView = function (options) {
     _this.render();
   };
 
+  /**
+   * Deselects all the items in `_this.content`. An implementing sub-class
+   * may want to override this method if selection is not done by toggling
+   * a "selected" class on the item element.
+   *
+   */
   _this.deselectAll = function () {
     Array.prototype.forEach.call(
-        _this.el.querySelectorAll('input[type=checkbox]'),
-        function (checkbox) {
-          checkbox.checked = false;
-        }
-      );
+      _this.content.querySelectorAll('input[type=checkbox]'),
+      function (checkbox) {
+        checkbox.checked = false;
+      }
+    );
   };
 
+  /**
+   * Frees resources associated with this view.
+   *
+   */
   _this.destroy = Util.compose(function () {
     _watchProperty = null;
 
@@ -39,7 +92,11 @@ var RadioOptionsView = function (options) {
     _this = null;
   }, _this.destroy);
 
-
+  /**
+   * Called when the collection is reset (primarily), or when items are added
+   * to/removed from the collection. Renders the content.
+   *
+   */
   _this.render = function () {
     var items,
         list;
@@ -56,10 +113,10 @@ var RadioOptionsView = function (options) {
         li.classList.add(_watchProperty);
         li.setAttribute('data-id', item.id);
         li.innerHTML = '<input type="checkbox"' +
-              ' id="' + _watchProperty + '-' +item.id + '"' +
+              ' id="' + _watchProperty + '-' + item.id + '"' +
               ' value="' + item.id + '"' +
               ' name="' + _watchProperty + '" />' +
-            '<label for="id-' + _watchProperty + '-' + item.id + '">' +
+            '<label for="' + _watchProperty + '-' + item.id + '">' +
               item.name +
             '</label>';
       });
@@ -73,7 +130,15 @@ var RadioOptionsView = function (options) {
     }
   };
 
-  // select multiple options
+  /**
+   * Method to update an element in `_this.content`, whose id
+   * matches the given value "id" attribute, to appear selected.
+   *
+   * @param value Array<{Mixed}>
+   *     Typically an array of objects with an "id" attribute which corresponds
+   *     to the "id" attribute of some input in `_this.content`.
+   *
+   */
   _this.setSelected = function (objs) {
     var id,
         el;
@@ -90,7 +155,7 @@ var RadioOptionsView = function (options) {
     // select each checkbox
     Array.prototype.forEach.call(objs, function (obj) {
       id = obj.id;
-      el = _this.el.querySelector('#' + _watchProperty + '-' + id);
+      el = _this.content.querySelector('#' + _watchProperty + '-' + id);
 
       if (el) {
         el.checked = true;
--- src/htdocs/js/settings/RadioOptionsView.js
+++ src/htdocs/js/settings/RadioOptionsView.js
@@ -5,7 +5,36 @@ var GenericCollectionView = require('core/GenericCollectionView'),
 
 var _DEFAULTS = {};
 
-
+/**
+ * Serves as a resuable view to display configuration options. This view
+ * accepts (among other things) a collection and a model on the configuration
+ * options at time of construction.
+ *
+ * The collection provides a list of all items for the collection view to
+ * render. The view listens to "add", "remove", and "reset" events on the
+ * provided collection and calls the render method when such events occur.
+ * The view does not listen to select/deselect on the collection...
+ *
+ * The model provides information regarding the currently selected values
+ * for various properties. By default, this view only listens for changes to
+ * the configured `options.watchProperty` property, and when that property
+ * changes, the view calls its `onEvent` method.
+ *
+ * All messaging to/from this view should occur view events/method calls on
+ * the provided model.
+ *
+ * Implementing sub-classes may override any public method for customized
+ * behavior, however, typical extension points may involve overriding the
+ * following methods:
+ *
+ * - deselectAll
+ * - setSelected
+ *
+ * @param options {Object}
+ *     Configuration options for this view. See documentation on the
+ *     `_initialize` method for complete details on all configuration
+ *     options that may be provided.
+ */
 var RadioOptionsView = function (options) {
   var _this,
       _initialize,
@@ -16,6 +45,20 @@ var RadioOptionsView = function (options) {
   _this = GenericCollectionView(options);
   options = Util.extend({}, _DEFAULTS, options);
 
+
+  /**
+   * Constructor.
+   *
+   * @param options {Object}
+   *     Configuration options for this view.
+   * @param options.collection {mvc/Collection}
+   *     The collection for this view.
+   * @param options.model {mvc/Model}
+   *     The model for this view.
+   * @param options.watchProperty {String}
+   *     The name of the property on the model to watch for changes and
+   *     subsequently trigger `onEvent`.
+   */
   _initialize = function (options) {
     // defines which setting to configure
     _watchProperty = options.watchProperty || '';
@@ -23,14 +66,25 @@ var RadioOptionsView = function (options) {
     _this.render();
   };
 
+  /**
+   * Deselects all the items in `_this.content`. An implementing sub-class
+   * may want to override this method if selection is not done by toggling
+   * a "selected" class on the item element.
+   *
+   */
   _this.deselectAll = function () {
-    Array.prototype.forEach.call(_this.el.querySelectorAll('input[type=radio]'),
+    Array.prototype.forEach.call(
+      _this.content.querySelectorAll('input[type=radio]'),
       function (radio) {
         radio.checked = false;
       }
     );
   };
 
+  /**
+   * Frees resources associated with this view.
+   *
+   */
   _this.destroy = Util.compose(function () {
     _watchProperty = null;
 
@@ -38,6 +92,11 @@ var RadioOptionsView = function (options) {
     _this = null;
   }, _this.destroy);
 
+  /**
+   * Called when the collection is reset (primarily), or when items are added
+   * to/removed from the collection. Renders the content.
+   *
+   */
   _this.render = function () {
     var items,
         list;
@@ -71,7 +130,15 @@ var RadioOptionsView = function (options) {
     }
   };
 
-  // Select one option
+  /**
+   * Method to update an element in `_this.content`, whose id
+   * matches the given value "id" attribute, to appear selected.
+   *
+   * @param value {Mixed}
+   *     Typically an object with an "id" attribute which corresponds to the
+   *     "id" attribute of some input in `_this.content`.
+   *
+   */
   _this.setSelected = function (obj) {
     var id,
         el;
@@ -81,7 +148,7 @@ var RadioOptionsView = function (options) {
     }
 
     id = obj.id;
-    el = _this.el.querySelector('#' + _watchProperty + '-' + id );
+    el = _this.content.querySelector('#' + _watchProperty + '-' + id );
 
     if (el) {
       el.checked = true;
