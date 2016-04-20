b461606df07b9d08e6ee29883c0eec561a54792d
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2016-04-20 08:24:42 -0600

    added markup

--- src/htdocs/js/list/ListViewFooter.js
+++ src/htdocs/js/list/ListViewFooter.js
@@ -21,6 +21,39 @@ var ListViewFooter = function (options) {
     _this = null;
   }, _this.destroy);
 
+  _this.listFooterMarkup = function () {
+    var buf,
+        el;
+
+    el = document.createElement('div');
+    el.className = 'list-view-footer';
+
+    buf = [];
+
+    buf.push(
+      '<h5>Didn\'t find what you were looking for?</h5>',
+      '<ul class="help no-style">',
+        '<li>',
+          'Check your &ldquo;Settings&rdquo;.',
+        '</li>',
+        '<li>',
+          '<a href="/earthquakes/map/doc_whicheqs.php">',
+            'Which earthquakes are included on the map and ',
+            'list?',
+          '</a>',
+        '</li>',
+        '<li>',
+          '<a href="/earthquakes/eventpage/unknown#impact_tellus">',
+            'Felt something not shown – report it here.',
+          '</a>',
+        '</li>',
+      '</ul>'
+    );
+
+    el.innerHTML = buf.join('');
+    return el;
+  };
+
   _initialize(options);
   options = null;
   return _this;
