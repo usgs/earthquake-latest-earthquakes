6d2cd94b98ee8915aba62b1b41c34daadc5eb4ec
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2016-04-27 14:26:25 -0600

    setup modes view, fixed createCollectionItemContent

--- src/htdocs/js/modes/ModesView.js
+++ src/htdocs/js/modes/ModesView.js
@@ -1,7 +1,7 @@
 'use strict';
 
-var Util = require('util/Util'),
-    View = require('mvc/View');
+var GenericCollectionView = require('core/GenericCollectionView'),
+    Util = require('util/Util');
 
 var _DEFAULTS = {
 
@@ -11,27 +11,87 @@ var ModesView = function (options) {
   var _this,
       _initialize;
 
-  _this = View(options);
-
-  _initialize = function (options) {
-    options = Util.extend({}, _DEFAULTS, options);
-  }
+  options = Util.extend({}, _DEFAULTS, options);
+  _this = GenericCollectionView(options);
 
   /**
    * Destroy all the things.
    */
-   _this.destroy = Util.compose(function () {
-     _initialize = null;
-     _this = null;
-   }, _this.destroy);
+  _this.destroy = Util.compose(function () {
+   _initialize = null;
+   _this = null;
+  }, _this.destroy);
 
-   _this.render = function () {
+  _this.createCollectionItemContent = function (obj) {
+    var icon;
 
-   };
+    icon = document.createElement('i');
+    icon.classList.add('material-icons');
+    icon.setAttribute('title', obj.title);
+    icon.innerHTML = obj.icon;
 
-   _initialize(options);
-   options = null;
-   return _this;
+    return icon;
+  };
+
+  _this.setSelected = function (objs) {
+    var el,
+        id;
+
+    if (!objs) {
+      return;
+    }
+
+    objs.forEach(function (obj) {
+      id = obj.id;
+      el = _this.content.querySelector('[data-id="' + id + '"]');
+
+      if (el) {
+        el.classList.add('selected');
+      }
+    });
+  };
+
+  /**
+   * Update model based on newly clicked item in the options view. If
+   * the clicked item was previously set as a value on the `watchProperty` for
+   * `_this.model` then that item is removed from the `watchProperty` value;
+   * otherwise the item is added to the `watchProperty` value.
+   *
+   * This method is called by onContentClick.
+   *
+   * @param obj {Object}
+   *     Configuration option that was clicked
+   */
+  _this.updateModel = function (obj) {
+    var index,
+        properties,
+        toSet;
+
+    toSet = {};
+    properties = _this.model.get(_this.watchProperty);
+
+    if (properties) {
+      toSet[_this.watchProperty] = properties.slice(0);
+    } else {
+      toSet[_this.watchProperty] = [];
+    }
+
+    // check model already contains selected object
+    index = toSet[_this.watchProperty].indexOf(obj);
+    if (index === -1) {
+      // does not contain object, add it
+      toSet[_this.watchProperty].push(obj);
+    } else {
+      // contains object, remove it
+      toSet[_this.watchProperty].splice(index, 1);
+    }
+
+    _this.model.set(toSet);
+  };
+
+
+  options = null;
+return _this;
 };
 
 module.exports = ModesView;
