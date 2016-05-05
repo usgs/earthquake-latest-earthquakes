24059f116856561e1f66bae0b744bae883dcd65e
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2016-05-05 13:33:49 -0600

    changed from help to about

---
+++ test/spec/about/AboutViewTest.js
@@ -0,0 +1,30 @@
+/* global before, chai, describe, it */
+'use strict';
+
+var AboutView = require('about/AboutView');
+
+var expect = chai.expect;
+
+describe('about/AboutView', function () {
+  describe('constructor', function () {
+    it('is a function', function () {
+      expect(typeof AboutView).to.equal('function');
+    });
+  });
+
+  describe('About View', function () {
+    var view;
+
+    view = AboutView();
+
+    view.render();
+
+    it('Creates markup as expected', function () {
+      expect(view.el).to.not.equal('');
+      expect(view.el.querySelectorAll('ul').length).to.equal(3);
+      expect(view.el.querySelectorAll('li').length).to.equal(12);
+      expect(view.el.querySelectorAll('i').length).to.equal(4);
+      expect(view.el.querySelectorAll('img').length).to.equal(3);
+    });
+  });
+});
