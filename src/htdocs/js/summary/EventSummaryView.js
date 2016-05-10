99132ae4206bf0926e5cec0258e45d7c24775b2c
ehunter-usgs <ehunter@usgs.gov>
2016-05-10 12:00:47 -0600

    Add close button to EventSummaryView. Update styles for EventSummaryView. Reset event property on model when EventSummaryView is closed.

--- src/htdocs/js/summary/EventSummaryView.js
+++ src/htdocs/js/summary/EventSummaryView.js
@@ -11,6 +11,7 @@ var EventSummaryView = function (options) {
   var _this,
       _initialize,
 
+      _closeButton,
       _formatter,
       _isVisible;
 
@@ -19,17 +20,26 @@ var EventSummaryView = function (options) {
   _initialize = function (options) {
     options = Util.extend({}, _DEFAULTS, options);
 
+    _closeButton = document.createElement('button');
+    _closeButton.classList.add('summary-close');
+    _closeButton.innerHTML = 'x';
+
     _formatter = options.formatter || EventSummaryFormat();
     _isVisible = false;
     _this.catalog = options.catalog || Collection();
 
-    _this.model.on('change:event', _this.render, _this);
-    // remove this
+    _this.model.off('change', 'render', _this);
+    _this.model.on('change:event', _this.onEventSelect, _this);
+    _closeButton.addEventListener('click', _this.hideEventSummary);
+
+    // remove this v v
     _this.catalog.on('reset', _this.onCatalogReset, _this);
+    // remove that ^ ^
   };
 
   _this.destroy = Util.compose(function () {
-    _this.model.off('change:event', _this.render, _this);
+    _this.model.off('change:event', _this.onEventSelect, _this);
+    _closeButton.off('click', _this.hideEventSummary, _this);
 
     _isVisible = null;
     _initialize = null;
@@ -38,29 +48,56 @@ var EventSummaryView = function (options) {
 
   // TODO, remove this. It is only to set a selected event
   _this.onCatalogReset = function () {
-    _this.model.set({
-      'event': _this.catalog.data()[3]
-    });
+    var eqs;
+
+    eqs = _this.catalog.data();
+    if (eqs.length !== 0) {
+      _this.model.set({
+        'event': eqs[0]
+      });
+    }
   };
 
-  _this.render = function () {
+  /**
+   * Determine whether or not the view is already visible and call render
+   */
+  _this.onEventSelect = function () {
     var eq;
 
     eq = _this.model.get('event');
 
     if (!eq) {
-      _this.el.innerHTML = '';
-      _this.el.classList.remove('show');
-      _isVisible = false;
+      _this.hideEventSummary();
       return;
     }
 
     if (!_isVisible) {
-      _this.el.classList.add('show');
-      _isVisible = true;
+      _this.showEventSummary(eq);
     }
 
+    _this.render(eq);
+  };
+
+  _this.deselectEvent = function () {
+    _this.model.set({
+      'event': null
+    });
+  };
+
+  _this.hideEventSummary = function () {
+    _this.deselectEvent();
+    _this.el.classList.remove('show');
+    _isVisible = false;
+  };
+
+  _this.showEventSummary = function () {
+    _this.el.classList.add('show');
+    _isVisible = true;
+  };
+
+  _this.render = function (eq) {
     _this.el.innerHTML = '';
+    _this.el.appendChild(_closeButton);
     _this.el.appendChild(_formatter.format(eq));
   };
 
