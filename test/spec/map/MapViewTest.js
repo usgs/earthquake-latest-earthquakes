09678c64fd04d72d76d36192f629853ff9a45a1a
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2016-05-18 15:41:47 -0600

    created test

---
+++ test/spec/map/MapViewTest.js
@@ -0,0 +1,20 @@
+/* global chai, describe, it */
+'use strict';
+
+var MapView = require('map/MapView');
+
+var expect = chai.expect;
+
+describe('map/Mapview', function () {
+  describe('constructor', function () {
+    it('Can be required', function () {
+      /* jshint -W030 */
+      expect(MapView).to.not.be.null;
+      /* jshint +W030 */
+    });
+  });
+
+  describe('', function () {
+
+  });
+});
