212c353f0130b04bd5a8f14823cafbf36eb72466
ehunter-usgs <ehunter@usgs.gov>
2016-08-10 11:21:21 -0600

    Call renderMapPositionChange directly from renderViewModesChange. Set flags to false within their render methods

--- src/htdocs/js/map/MapView.js
+++ src/htdocs/js/map/MapView.js
@@ -349,34 +349,22 @@ var MapView = function (options) {
   };
 
   _this.render = function (force) {
+    //console.log('render');
+
     if (_renderViewModesChange || force === true) {
       _this.renderViewModesChange();
-      _renderViewModesChange = false;
-
-      // ensure map is updated when view mode changes and map is visible
-      if (_this.isEnabled()) {
-        _renderMapPositionChange = true;
-      }
-
-      // focus map on selected event when "map" is enabled as a view mode
-      if (_this.isEnabled() && _this.model.get('event')) {
-        _this.onChangeEvent();
-      }
     }
 
     if (_renderBasemapChange || force === true) {
       _this.renderBasemapChange();
-      _renderBasemapChange = false;
     }
 
     if (_renderOverlayChange || force === true) {
       _this.renderOverlayChange();
-      _renderOverlayChange = false;
     }
 
     if (_renderMapPositionChange || force === true) {
       _this.renderMapPositionChange();
-      _renderMapPositionChange = false;
     }
   };
 
@@ -387,6 +375,7 @@ var MapView = function (options) {
     oldBasemap = _this.basemap;
     newBasemap = _this.model.get('basemap');
     _this.basemap = newBasemap; // keep track of selected basemap
+    _renderBasemapChange = false;
 
     // if basemap is already selected, do nothing
     if (oldBasemap && newBasemap && oldBasemap.id === newBasemap.id) {
@@ -408,6 +397,11 @@ var MapView = function (options) {
     var mapBounds,
         modelBounds;
 
+
+    console.log('map position');
+    _renderMapPositionChange = false;
+
+
     if (_this.isEnabled()) {
       modelBounds = _this.model.get('mapposition');
 
@@ -422,6 +416,8 @@ var MapView = function (options) {
         _this.map.fitBounds(modelBounds, {animate: false});
       }
     }
+
+    _renderMapPositionChange = false;
   };
 
   _this.renderOverlayChange = function () {
@@ -433,6 +429,7 @@ var MapView = function (options) {
     oldOverlays = _this.overlays.slice(0);
     newOverlays = _this.model.get('overlays') || [];
     _this.overlays = newOverlays; // keep track of selected overlays
+    _renderOverlayChange = false;
 
     // remove overlays that no longer exist in selection
     for (i = 0; i < oldOverlays.length; i++) {
@@ -452,11 +449,21 @@ var MapView = function (options) {
   };
 
   _this.renderViewModesChange = function () {
+
+    console.log('view modes');
+    _renderViewModesChange = false;
+
     if (_this.isEnabled()) {
       if (!_this.hasBounds()) {
         _this.ignoreNextMoveEnd = true;
       }
       _this.map.invalidateSize();
+      // ensure map is updated when view mode changes and map is visible
+      _this.renderMapPositionChange();
+      // focus map on selected event when "map" is enabled as a view mode
+      if (_this.model.get('event')) {
+        _this.onChangeEvent();
+      }
     }
   };
 
