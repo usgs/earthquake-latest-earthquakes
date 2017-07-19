e8aeedd0a71c77b01a29fd9e05d0278a29bd8888
ehunter-usgs <ehunter@usgs.gov>
2017-07-19 16:39:10 -0600

    Format MetadataView

--- src/htdocs/js/list/MetadataView.js
+++ src/htdocs/js/list/MetadataView.js
@@ -10,6 +10,7 @@ var Accordion = require('accordion/Accordion'),
     ModalView = require('mvc/ModalView'),
     Util = require('util/Util');
 
+
 var _DEFAULTS = {
   noDataMessage: 'There are no events in the current feed.',
 };
@@ -27,12 +28,13 @@ var MetadataView = function (options) {
   _this = GenericCollectionView(options);
 
   /**
-   * Constructor. Initializes a new DownLoadView.
+   * Constructor. Initializes a new MetadataView.
    *
    * @params options {Object}
    *    Configuration options. May Include:
-   * @params options.model
    * @params options.collection
+   * @params options.formatter
+   * @params options.model
    */
   _initialize = function (options) {
     _this.collection = options.collection || Collection();
@@ -42,9 +44,51 @@ var MetadataView = function (options) {
     _this.model.off('change', 'render', _this);
     _this.model.on('change:feed', 'displaySearchParameters', _this);
 
-    _this.createSkeleton();
-  };
+    // TODO, make configurable, to start open
+    _this.el.classList.add('accordion');
+    _this.el.classList.add('accordion-closed');
+    _this.el.innerHTML =
+      '<h3 class="header-title"></h3>' +
+      '<span class="header-count accordion-toggle"></span>' +
+      '<div class="accordion-content header-info-content">' +
+        '<h4 class="download-title"></h4>' +
+        '<dl>' +
+          '<dt>Last Updated</dt>' +
+          '<dd class="feed-update-time"></dd>' +
+        '</dl>' +
+        '<button class="download-button blue" type="button">Download</button>' +
+        '<div class="search-parameter-view"><div>' +
+      '</div>';
+    _this.countEl = _this.el.querySelector('.header-count');
+    _this.downloadButtonEl = _this.el.querySelector('.download-button');
+    _this.downloadButtonEl.addEventListener('click', _this.onButtonClick);
+    _this.downloadTitleEl = _this.el.querySelector('.download-title');
+    _this.feedUpdateTimeEL = _this.el.querySelector('.feed-update-time');
+    _this.searchButton = document.createElement('button');
+    _this.searchButton.addEventListener('click', _this.onSearchButtonClick, _this);
+    _this.searchButton.classList.add('search-button');
+    _this.searchButton.classList.add('blue');
+    _this.searchButton.innerHTML = 'Modify Search';
+    _this.searchParameterViewEl =
+        _this.el.querySelector('.search-parameter-view');
+    _this.titleEl = _this.el.querySelector('.header-title');
 
+    // Create Accordion
+    Accordion({
+      el: _this.el
+    });
+
+    // Create DownloadView
+    _this.downloadView = DownloadView({
+      model: _this.model,
+      collection: _this.collection
+    });
+
+    // Create Modal for DownloadView
+    _this.downloadModal = ModalView(_this.downloadView.el, {
+      title: 'Download'
+    });
+  };
 
   /**
    * Frees resources associated with this view.
@@ -61,59 +105,40 @@ var MetadataView = function (options) {
     _this = null;
   }, _this.destroy);
 
-
-  _this.createSkeleton = function () {
-    _this.el.classList.add('accordion');
-    // TODO, make configurable, to start open
-    _this.el.classList.add('accordion-closed');
-    _this.el.innerHTML =
-      '<h3 class="header-title"></h3>' +
-      '<span class="header-count accordion-toggle"></span>' +
-      '<div class="accordion-content header-info-content">' +
-        '<h4 class="download-title"></h4>' +
-        '<dl>' +
-          '<dt>Last Updated</dt>' +
-          '<dd class="feed-update-time"></dd>' +
-        '</dl>' +
-        '<button class="download-button blue" type="button">Download</button>' +
-        '<div class="search-parameter-view"><div>' +
-      '</div>';
-
-    Accordion({
-      el: _this.el
-    });
-
-    _this.titleEl = _this.el.querySelector('.header-title');
-    _this.downloadTitleEl = _this.el.querySelector('.download-title');
-    _this.countEl = _this.el.querySelector('.header-count');
-    _this.downloadButtonEl = _this.el.querySelector('.download-button');
-    _this.feedUpdateTimeEL = _this.el.querySelector('.feed-update-time');
-    _this.searchParameterViewEl =
-        _this.el.querySelector('.search-parameter-view');
-
-    _this.downloadView = DownloadView({
-      model: _this.model,
-      collection: _this.collection
-    });
-
-    _this.downloadModal = ModalView(_this.downloadView.el, {
-      title: 'Download'
-    });
-
-    _this.downloadButtonEl.addEventListener('click', _this.onButtonClick);
-  };
-
-
   /**
-   * Shows download view when button is clicked.
+   * Loop through all search parameters and display with
+   * "modify search" button
    */
-  _this.onButtonClick = function () {
-    _this.downloadModal.show();
+  _this.displaySearchParameters = function () {
+    var buf,
+        feed,
+        key,
+        params;
+
+    feed = this.model.get('feed') || {};
+
+    // only display if feed is a search
+    if (feed.isSearch) {
+      buf = [];
+      params = feed.params;
+
+      for (key in params) {
+        buf.push(
+          '<dt>' + key + '</dt>' +
+          '<dd>' + params[key] + '</dd>'
+        );
+      }
+
+      // Add search parameters with search button
+      _this.searchParameterViewEl.innerHTML =
+          '<h4>Search Parameters</h4>' +
+          '<dl class="search-parameter-list">' + buf.join('') + '</dl>';
+      _this.searchParameterViewEl.appendChild(_this.searchButton);
+    }
   };
 
-
   /**
-   * Formats earthquake count information
+   * Format earthquake count information
    *
    * @param number (totalCount)
    *    number of total earthquakes.
@@ -154,7 +179,21 @@ var MetadataView = function (options) {
   };
 
   /**
-   * Renders the view.
+   * Show download view when button is clicked.
+   */
+  _this.onButtonClick = function () {
+    _this.downloadModal.show();
+  };
+
+  /**
+   * Redirect to earthquake search page when search button is clicked
+   */
+  _this.onSearchButtonClick = function () {
+    window.location = SEARCH_PATH + window.location.hash;
+  };
+
+  /**
+   * Render the view.
    */
   _this.render = function () {
     var displayCount,
@@ -177,48 +216,6 @@ var MetadataView = function (options) {
     _this.countEl.innerHTML = headerCount;
 
     _this.displaySearchParameters();
-  };
-
-  _this.displaySearchParameters = function () {
-    var buf,
-        feed,
-        key,
-        params;
-
-    feed = this.model.get('feed') || {};
-
-    // only display if feed is a search
-    if (feed.isSearch) {
-      buf = [];
-      params = feed.params;
-
-      for (key in params) {
-        buf.push(
-          '<dt>' + key + '</dt>' +
-          '<dd>' + params[key] + '</dd>'
-        );
-      }
-
-      _this.searchParameterViewEl.innerHTML = '<h4>Search Parameters</h4>' +
-          '<dl class="search-parameter-list">' + buf.join('') + '</dl>';
-
-      if (!_this.searchButton) {
-        _this.renderSearchButton();
-      }
-      _this.searchParameterViewEl.appendChild(_this.searchButton);
-    }
-  };
-
-  _this.renderSearchButton = function () {
-    _this.searchButton = document.createElement('button');
-    _this.searchButton.addEventListener('click', _this.onSearchButtonClick, _this);
-    _this.searchButton.classList.add('search-button');
-    _this.searchButton.classList.add('blue');
-    _this.searchButton.innerHTML = 'Modify Search';
-  };
-
-  _this.onSearchButtonClick = function () {
-    window.location = SEARCH_PATH + window.location.hash;
   };
 
 
